9670d22723ff65d9fefd5c372bef052bd838fcfd
Richard Oh <n24512@protonmail.com>
2021-10-08 00:30:50 -0500

    setup index.js, server.js, routers, downloaded all dependencies, setup scripts

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,7 @@
-// build your `/api/resources` router here
+// build your `/api/projects` router here
+const express = require("express");
+const Resources = require("./model");
+
+const router = express.Router();
+
+module.exports = router;
